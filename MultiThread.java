b5cd646e9710a9383ec6fa8f24e52da46e96fc59
Vladian-Diac-JCMaxwell-4Bi <nicu.diac@jcmaxwell.it>
2017-04-28 23:00:39 +0200

    Update MultiThread.java

--- MultiThread.java
+++ MultiThread.java
@@ -2,77 +2,78 @@
 
 package multithread;
 
-import java.util.concurrent.TimeUnit;
-import static multithread.TicTacToe.contatore;
+import java.util.concurrent.TimeUnit; // importo pacchetto 
+import static multithread.TicTacToe.contatore;  // importo pacchetto per la visuliazzazione del punteggio 
+ 
 
+public class MultiThread { // classe MultiThread dichiarata
 
-public class MultiThread {
-
-    public static void main(String[] args) {
-        System.out.println("Main Thread iniziata...");
-        long start = System.currentTimeMillis();
+    public static void main(String[] args) {  // classe principale d esecuzione Main
+        System.out.println("Main Thread iniziata...");  // visualizzo output, che il nostro pogramma è inizziato 
+        long start = System.currentTimeMillis(); // assegno a start il tempo iniziale in cui è stato eseguito il programma
         
-        Thread tic = new Thread (new TicTacToe("TIC"));
-        tic.start();
+        Thread tic = new Thread (new TicTacToe("TIC")); // creo un nuovo Thread è lo chiamo Tic con output TIC
+        tic.start(); // avvio il thread TIC
         
-        Thread tac = new Thread(new TicTacToe("TAC"));
-        tac.start();
+        Thread tac = new Thread(new TicTacToe("TAC")); // creo un nuovo Thread è lo chiamo Toc con output TOC
+        tac.start(); // avvio il thread TAC
         
-        Thread toe = new Thread (new TicTacToe("TOE"));
-        toe.start();
-           long end = System.currentTimeMillis();
-        System.out.println("Main Thread completata! tempo di esecuzione: " + (end - start) + "ms");
-        try{
-            tic.join();
-        }catch(InterruptedException e)
-        {System.out.println(e);}
-         try{
-            tac.join();
-        }catch(InterruptedException e)
-        {System.out.println(e);}
-         try{
-            toe.join();
-        }catch(InterruptedException e)
-        {System.out.println(e);}
+        Thread toe = new Thread (new TicTacToe("TOE")); // creo un nuovo Thread è lo chiamo TOE con output TOE
+        toe.start(); // avvio il thread TOE
+           long end = System.currentTimeMillis();  // assegno a end il tempo finale in cui è stato eseguito il programma
+        System.out.println("Main Thread completata! tempo di esecuzione: " + (end - start) + "ms"); // visualizzo poi alla fine il tempo totale che i thread hanno impiegato a concludere
+        try{ // provo 
+            tic.join();  // verifico quando tic ha concluso il Thread
+        }catch(InterruptedException e) // eccezione
+        {System.out.println(e);} // evventuale messaggio di errore
+         try{ // provo 
+            tac.join();  // verifico quando tac ha concluso il Thread
+        }catch(InterruptedException e) // eccezione
+        {System.out.println(e);} // evventuale messaggio di errore
+         try{ // provo 
+            toe.join(); // verifico quando toe ha concluso il Thread
+        }catch(InterruptedException e) // eccezione
+        {System.out.println(e);} // evventuale messaggio di errore
          
-         System.out.println("Punteggio:  -- >> "+ contatore);
+         System.out.println("Punteggio:  -- >> "+ contatore); // visualizzo il punteggio finale 
    
     }
     
 }
 
-class TicTacToe  implements Runnable {
+class TicTacToe  implements Runnable { // implemento la classe 
 
     
-    private String t;
+    private String t;  // dichiarazione variabili
     private String msg;
     private static boolean taC = false;
     public static int contatore = 0;
 
-    public TicTacToe (String s) {
+    public TicTacToe (String s) { // costruttore di TicTacToe
         this.t = s;
     }
     
-    @Override 
+    @Override // Annotazione per il compilatore se facessimo un overloading invece di un override il copilatore ci segnalerebbe l'errore
+
     
-    public void run() {
-     for (int i = 10; i > 0; i--) 
+    public void run() { // classe
+     for (int i = 10; i > 0; i--)  // ciclo for
         {           
-            if("TAC".equals(t))
-                taC = true;
+            if("TAC".equals(t)) // se tac è uguale all'output T
+                taC = true; // allora taC è uguale a vero
                 
-            msg = "<" + t + "> ";
+            msg = "<" + t + "> ";   // assegno a msg il seguente testo
             int casuale=100+(int)(Math.random()*300); //Tempo casuale tra 100 e 300 millisecondi
-             try {
-                TimeUnit.MILLISECONDS.sleep(casuale);
-            } catch (InterruptedException e) {}
-            if("TOE".equals(t) && taC == true)
-                contatore++;
-            else
-                taC = false;
-            msg += t + ": " + i;
+             try { // provo
+                TimeUnit.MILLISECONDS.sleep(casuale); // tempo random per la viosualizzazione random
+            } catch (InterruptedException e) {} // eccezione
+            if("TOE".equals(t) && taC == true) // se Toe è uguale a T e taC è uguale a vero 
+                contatore++; // allora increamento il punteggio di +1
+            else // altrimenti
+                taC = false;  // tac diventa falso0
+            msg += t + ": " + i;  // assegno a msg il seguente testo
             
-            System.out.println(msg);
+            System.out.println(msg);  // visualizzo mesaggio di ouput
     }
             
     }
